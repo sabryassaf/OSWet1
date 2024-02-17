2a616da03690533304132637d454c0dbe2b38e74
Mary Bayyouk <mary@bayyouk.com>
2024-02-17 14:21:56 +0200

    PipeCommand implementation

---
+++ cmake-build-debug/CMakeFiles/skeleton_smash.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skeleton_smash.dir/Commands.cpp.obj"
+  "CMakeFiles/skeleton_smash.dir/signals.cpp.obj"
+  "CMakeFiles/skeleton_smash.dir/smash.cpp.obj"
+  "libskeleton_smash.dll.a"
+  "skeleton_smash.exe"
+  "skeleton_smash.exe.manifest"
+  "skeleton_smash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skeleton_smash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
